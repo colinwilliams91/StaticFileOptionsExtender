8ad3139bb1a4aa75083329fff088519031191257
Colin Williams <colin.williams.dev@gmail.com>
2024-07-06 00:28:32 -0500

    test!: add formatting for ;;; in changelog to remote link to release tag branch

--- Scripts/PreprocessChangelog.ps1
+++ Scripts/PreprocessChangelog.ps1
@@ -1,8 +1,19 @@
+param (
+    [string]$Version
+)
+
 $changelogPath = "$PSScriptRoot\..\Docs\CHANGELOG.md"
 $outputPath = "$PSScriptRoot\..\Docs\CHANGELOG_PLAIN.txt"
 
 if (Test-Path $changelogPath) {
     $content = Get-Content $changelogPath
+    $contentForLinks = $content
+
+    # Updating Docs\CHANGELOG.md with links to remote tags
+    $contentForLinks = $contentForLinks -replace ';;;', "./tree/$Version"
+
+    # Write the processed content to a new file
+    Set-Content -Path $changelogPath -Value $contentForLinks -Force
 
     # Simple example: replace markdown headers with plain text
     $content = $content -replace '### ', '--- '
